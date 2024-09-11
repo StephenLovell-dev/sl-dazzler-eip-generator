e69ab103716206697d0f09d187a69bd09263b121
Stephen Lovell <stephen.lovell@bbc.co.uk>
2024-09-11 17:38:10 +0100

    fix: Test commit to new main

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,5 +1,5 @@
 from src.dazzler_interstitials.main import main
-
+# TODO Lots to do
 def lambda_handler(event, context):
     region = 'eu-west-1'
     account = context.invoked_function_arn.split(':')[4]
