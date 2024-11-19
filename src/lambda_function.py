44e06415930a69e9c963975f404046280e369221
Stephen Lovell <stephen.lovell@bbc.co.uk>
2024-11-19 16:31:35 +0000

    fix: remove src folder

--- src/lambda_function.py
+++
@@ -1,6 +0,0 @@
-from src.dazzler_interstitials.main import main
-# TODO Lots to do
-def lambda_handler(event, context):
-    region = 'eu-west-1'
-    account = context.invoked_function_arn.split(':')[4]
-    main(event, account)
